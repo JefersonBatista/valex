62650df7851e1106e12b8f5fca3342f784c9d651
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-04-14 14:38:12 -0300

    Setup basic infrastructure

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+const port = process.env.PORT;
+app.listen(port, () => {
+	console.log(`Server listening on port ${process.env.PORT}`);
+});
--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,6 @@
-import dotenv from "dotenv";
 import pg from "pg";
-dotenv.config();
 
 const { Pool } = pg;
 export const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL,
 });
